1d32b96fd68a33af38edd870a77906a06933f3ad
Parnika <parnika.chaturvedi@gmail.com>
2025-04-16 19:36:55 -0700

    login page UI

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -7,6 +7,7 @@ import "../App.css"
 const AuthForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isHovered, setIsHovered] = useState(false);
 
   const signUp = async () => {
     try {
@@ -28,10 +29,34 @@ const AuthForm = () => {
 
   return (
     <div>
-      <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email" />
-      <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="password" />
-      <button onClick={signUp}>Sign Up</button>
-      <button onClick={logIn}>Log In</button>
+      <div>
+        <h2 style={{ margin: '25px' }}> Welcome to <span style={{ color: '#1db954' }}>Friendify</span>! </h2>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email" />
+        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="password" />
+      </div>
+      <div>
+        <button onClick={logIn} style={{ margin: '10px' }}>Log In</button>
+        {/* <button onClick={signUp} style={{ margin: '10px' }}>Sign Up</button> */}
+      </div>
+      <div>
+        <p style={{ margin: '10px' }}> <strong>
+          Don't have an account?{' '}
+          <span 
+            style={{
+              cursor: 'pointer',
+              color: isHovered ? 'white' : '#1db954',
+              transform: isHovered ? 'scale(1.05)' : 'none',
+              display: 'inline-block',
+              transition: 'transform 0.2s ease, color 0.2s ease',
+            }}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            onClick={signUp}
+          >
+            Sign Up!
+          </span>
+        </strong> </p>
+      </div>
     </div>
   );
 };
